55a894d4a2b564ec7557d74e2977194d7099005f
flow <flow@hrmm.xyz>
2025-07-07 15:34:56 +0900

    middleware

---
+++ middleware.ts
@@ -0,0 +1,1 @@
+export default function middleware() { console.log("hi") }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react(), vercel({
+  })],
 })
